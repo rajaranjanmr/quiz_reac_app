d118f8ccd645e4af3a36ac8cefd474cec84be51d
raranjan <raja.ranjan@genesys.com>
2022-05-07 16:58:27 +0530

    removing console and commented code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,7 +12,6 @@ function Home(){
                     method: "GET",
                     url: "https://opentdb.com/api_category.php",
                   });
-                  console.log("its response",response.data.trivia_categories)
                   dispatch({type: "GET_CATEGORY_LIST",
                   payload: response.data.trivia_categories,})
             }
@@ -23,7 +22,6 @@ function Home(){
         }
         getCategories()
     },[]);
-    console.log("its dispatch data",state)
 
     return(
         <>
@@ -34,21 +32,6 @@ function Home(){
            state.categories.map((x)=> <Category value={x}/>):console.log("ites empty")
             }
             
-            {/* <div class="category cat-two">
-                <div class="category-content">States Based</div>
-                <a class="button-category" href="quizcat.html">Let's Begin</a>
-
-            </div>
-            <div class="category cat-three">
-                <div class="category-content">Programming based</div>
-                <a class="button-category" href="quizcat.html">Let's Begin</a>
-
-            </div>
-            <div class="category cat-four">
-                <div class="category-content cat5">Politics Based</div>
-                <a class="button-category" href="quizcat.html">Let's Begin</a>
-
-            </div> */}
         </div>
         </>
     )
